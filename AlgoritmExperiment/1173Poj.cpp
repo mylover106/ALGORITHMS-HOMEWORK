1c2f53e0fcbc0991f210dab521ee99eb23a2da0a
mylover106 <1066616102@qq.com>
2018-03-23 21:41:04 +0800

    1173Poj accepted

--- AlgoritmExperiment/1173Poj.cpp
+++ AlgoritmExperiment/1173Poj.cpp
@@ -1,68 +1,98 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int length, bar_num, max_width, num_test, sum = 0;
-// string zero("00000000000000000000000000000000000000000000000000000000000000");
-// string one("1111111111111111111111111111111111111111111111111111111111111111");
+const int Unsure = -1;
+const int False  =  0;
 
-void recursive_find_k(bool color, int indix, int order[], string & now_s, int left, const string test[]) {
-    cout << now_s << endl;
-    if (left == 0) {
-        if (indix == length) {
-            sum++;
-            for (int i = 0; i < num_test; ++i) {
-                if (test[i] == now_s) {
-                    order[i] = sum;
-                }
-            }
-            return ;
-        } else {
-            return ;
+
+int num_search(int len, int num, int width, int bc[34][34]) {
+    if (len < num) return False;
+    else if (len > num * width) return False;
+    else if (len == 0 && num == 0) return 1;
+    else if (bc[len][num] == Unsure) {
+        bc[len][num] = False;
+        for (int i = 1; i <= width; ++i) {
+            bc[len][num] += num_search(len - i, num - 1, width, bc);
         }
     }
-    if (color == true)
-        for (int i = 0; i < max_width; ++i) {
-            // update the now_s
-            // now_s.replace(indix + now_s.begin(), i + 1, '1');
-            for (int j = 0; j < i + 1; ++j) {
-                now_s[j + indix] = '0';
-            }
-            now_s[i + indix] = true;
-            recursive_find_k(false, i + indix + 1, order, now_s, left - 1, test);
-        }
-    if (color == false)
-        for (int i = 0; i < max_width; ++i) {
-            // update the now_s
-            // now_s.replace(indix + now_s.begin(), max_width - i - 1, '0');
-            for (int j = 0; j < max_width - i - 1; ++j) {
-                now_s[j + indix] = '1';
-            }
-            // now_s[max_width - i - 1 + indix] = false;
-            recursive_find_k(true, max_width - i -1 + indix, order, now_s, left - 1, test);
-            // now_s[max_width]
-        }
+    return bc[len][num];
 }
 
 
+void normal(string s[], int ns[][34], int test_num, int num) {
+    for(int i = 0; i < test_num; ++i) {
+        int sum = 0;
+        int indix = 0;
+        for (int j = 0; j < s[i].size(); ++j) {
+            // cout << sum << endl;
+            if (j == 0) sum++;
+            else if (s[i][j] == s[i][j-1]) sum++;
+            else {
+                ns[i][indix++] = sum;
+                sum = 1;
+            }
+        }
+        ns[i][indix] = sum;
+    }
+}
+// if you want to use dynamic array use just one dimension
+
+
 int main() {
-    cin >> length >> bar_num >> max_width ;
-    cin >> num_test ;
-    string test_s[num_test] ;
-    int order[num_test];
-    for (int i = 0; i < num_test; ++i) {
-        cin >> test_s[i];
-    }
-    // for (int i = 0; i < num_test; ++i) {
-    //     cout << test_s[i] << endl;
+    int len, num, width, test_num;
+    cin >> len >> num >> width; 
+    cin >> test_num;
+    string s[test_num];
+    for (int i = 0; i < test_num; ++i)
+        cin >> s[i];
+    int ns[test_num][34];
+    int order_s[test_num];
+    int bc[34][34];
+    // init
+    for (int i = 0; i < test_num; ++i) 
+        for (int j = 0; j < num; ++j) 
+            ns[i][j] = 0;
+    for (int i = 0; i <= len; ++i) 
+        for (int j = 0; j <= len; ++j)
+            bc[i][j] = Unsure;
+    for (int i = 0; i < test_num; ++i) 
+        order_s[i] = 0;
+
+    // start search
+    int sum;
+    sum = num_search(len, num, width, bc);
+
+    // normal the input string data
+    normal(s, ns, test_num, num);
+    // for (int i = 0; i < test_num; ++i) {
+    //     for (int j = 0; j < num; ++j) 
+    //         cout << ns[i][j];
+    //     cout << endl;
     // }
-    string now_s(test_s[0]);
-    cout << "now_s" << endl;
-    recursive_find_k(true, 0, order, now_s, bar_num, test_s);
-    cout << sum << endl;
-    for (int i = 0; i < num_test; ++i) {
-        cout << order[i] << endl;
+    int indix_sum = 0;
+    for (int i = 0; i < test_num; ++i) {
+        for (int t = 0; t < num; ++t) {
+            indix_sum = 0;
+            for (int q = 0; q < t; ++q) {
+                indix_sum += ns[i][q];
+            }
+            if(t%2 == 0) {
+                for (int k = ns[i][t] - 1; k > 0; --k) {
+                    order_s[i] += num_search(len - k - indix_sum, num - t - 1, width, bc); 
+                }
+            }
+            if (t%2 == 1) {
+                for (int k = ns[i][t] + 1; k <= width; ++k) {
+                    order_s[i] += num_search(len - k - indix_sum, num - t - 1, width, bc);
+                }
+            }
+        }
     }
-    return 0;
+    // find the order 
+    cout << sum << endl;
+    for (int i = 0; i < test_num; ++i) 
+        cout << order_s[i] << endl;
 }
