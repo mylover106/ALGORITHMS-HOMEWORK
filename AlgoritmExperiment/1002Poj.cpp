92c0aecbfd3654ace15b0a2c8dbe6dd7d6fdda4c
mylover106 <1066616102@qq.com>
2018-03-28 18:30:06 +0800

    when I use G++, it is always run time error. but when I switch to C++ I got AC. Fucking OJ-POJ

--- AlgoritmExperiment/1002Poj.cpp
+++ AlgoritmExperiment/1002Poj.cpp
@@ -2,78 +2,49 @@
 #include <map>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <stdio.h>
 
 using namespace std;
 
 
 #define DONT_HAVE 0
 
-map <int, int> number_count;
-map <char, int> c_n = {
-    {'1', 1},
-    {'2', 2},
-    {'3', 3},
-    {'4', 4},
-    {'5', 5},
-    {'6', 6},
-    {'7', 7},
-    {'8', 8},
-    {'9', 9},
-    {'0', 0},
-    {'A', 2},
-    {'B', 2},
-    {'C', 2},
-    {'D', 3},
-    {'E', 3},
-    {'F', 3},
-    {'G', 4},
-    {'H', 4},
-    {'I', 4},
-    {'J', 5},
-    {'K', 5},
-    {'L', 5},
-    {'M', 6},
-    {'N', 6},
-    {'O', 6},
-    {'P', 7},
-    {'R', 7},
-    {'S', 7},
-    {'T', 8},
-    {'U', 8},
-    {'V', 8},
-    {'W', 9},
-    {'X', 9},
-    {'Y', 9}
-};
+map <string, int> number_count;
 
-vector <int> dpl_list;
+char char_lst[34] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y'};
+int int_lst[34] =  { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',   '2' , '2', '2', '3',  '3', '3',   '4', '4' , '4', '5','5', '5',  '6', '6', '6', '7', '7', '7', '8','8', '8', '9', '9','9' };
+
+map <char, char> c_n;
+
+vector <string> dpl_list;
 
 
-int s_number(string s) {
-    int sum = 0;
+string s_number(string s) {
+    string sum;
     string :: iterator i;
     for (i = s.begin(); i != s.end(); ++i) {
         if (c_n[*i] != DONT_HAVE || *i == '0') {
-            sum = sum * 10 + c_n[*i];
+            sum += c_n[*i];
         }
     }
+    sum.insert(3,"-");
     return sum;
 }
 
 
-string number_s(int number) {
-    // cout << number << endl;
-    string ans = to_string(number);
-    ans.insert(3,"-");
-    // cout << ans;
-    return ans;
-}
-
-
 int main() {
-    int number, n;
-    cin >> n;
+    string number;
+    int n;
+    scanf("%d", &n);
     string s;
+
+    // init the map
+    for (int i = 0; i < 34; ++i) {
+        c_n[char_lst[i]] = int_lst[i];
+    }
+
+
     for (int i = 0; i < n; ++i) {
         cin >> s;
         number = s_number(s);
@@ -89,8 +60,9 @@ int main() {
     sort(dpl_list.begin(), dpl_list.end());
 
     if (dpl_list.size() == 0) cout << "No duplicates." << endl;
-    else for (vector <int> :: iterator i = dpl_list.begin(); i != dpl_list.end(); ++i) {
-            cout << number_s(*i) << " " << number_count[*i] << endl;
+    else for (vector <string> :: iterator i = dpl_list.begin(); i != dpl_list.end(); ++i) {
+            // cout << *i << " " << number_count[*i] << endl;
+            printf("%s %d\n", (*i).c_str(), number_count[*i]);
         }
     return 0;
 }
